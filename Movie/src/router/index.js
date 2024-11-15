d515015780cf26172af87dec32c90d7881047aa7
BaeejiH <bjihun0511@gmail.com>
2024-11-15 19:18:20 +0900

    DB연결 + list

--- Movie/src/router/index.js
+++ Movie/src/router/index.js
@@ -1,42 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SubPage from '@/views/Main/SubPage.vue'
+import MainPage from '../views/Main/MainPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'SubPage',
+    component: SubPage
   },
   {
-    path: '/user',
-    name: 'ListView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ListView.vue')
-  },
-  {
-    path: '/user/findById/:name', // 주소처럼 인식해서 name의 데이터가 넘어감.
-    name: 'SelectView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/SelectView.vue')
-  },
-  {
-    path: '/user/editById/:no',
-    name: 'UpdateView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/UpdateView.vue')
-  },
-  {
-    path: '/user/save',
-    name: 'CreateView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/CreateView.vue')
-  },
-  {
-    path: '/Movie',
-    name: 'MovieList',
-    component: () => import(/* webpackChunkName: "about" */ '../views/MovieList.vue')
-  },
-
-  {
-    path: '/Movie/:movie_num',
-    name: 'DetailMovie',
-    component: () => import(/* webpackChunkName: "about" */ '../views/DetailMovie.vue')
+    path: '/main',
+    name: 'MainPage',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Main/MainPage.vue')
   },
 ]
 
---
+++ Movie/src/views/Main/SubPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link  to="/main">메인페이지로 가기</router-link>
+ </div>
+</template>
+
+<script>
+export default {
+ name:'SubPage',
+}
+</script>
+
+<style>
+
+</style>
